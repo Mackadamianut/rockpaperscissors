496b18851c9c92d15d8cafdae7985b518e5d5d50
Mackadamianut <macklin.iv@gmail.com>
2022-06-21 23:45:36 -0600

    add game function that plays 5 rounds and returns a win/lose message.

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let compScore = 0;
+
 function computerPlay() {
     const arrOfChoices = ["rock", "paper", "scissors"];
     const randomNum = Math.floor(Math.random() * 3);
@@ -13,16 +16,43 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection === 'paper' && computerSelection === 'paper') {
         return "You tied! You both picked Paper." // end of ties
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++
         return "You Win! Paper beats Rock."
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return "You Lose! Rock smashes Scissors." // end of vs rock
+        compScore++
+        return "You Lose! Rock smashes Scissors."
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore++
         return "You Win! You Rock smashes Scissors."
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        compScore++
         return "You Lose! Scissors cut Paper."// end of scissors
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        compScore++
         return "You Lose! Paper beats Rock."
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore++
         return "You Win! Scissors cut Paper." // end of paper
     }
 }
+
+
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt("Choose what to throw", "Rock, Paper, Scissors").toLowerCase();
+    const computerSelection = computerPlay(); 
+    console.log(playRound(playerSelection, computerSelection));
+    }
+
+    if (playerScore > compScore) {
+        return "You beat the computer! You are a genius!"
+    } else if (playerScore < compScore) {
+        return "You lost to the computer! You suck!"
+    } else {
+        return "You tied! Better luck next time."
+    }
+}
+
+console.log(game());
+
